3923438a02a82fda699af37954d0300ae818dc9a
Le_Benj <benjilanery@gmail.com>
2024-01-17 22:26:47 +0100

    Ajout de la feature de dessin

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './css/Notes.css';
+import { Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css'
+
 
 const toolbarOptions = [
   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
@@ -13,6 +16,11 @@ const toolbarOptions = [
 
 const Notes = ({ content, onContentChange, onDeleteNote }) => {
   const [editorHtml, setEditorHtml] = useState('');
+  const [isDrawingMode, setIsDrawingMode] = useState(false);
+
+  const handleToggleDrawingMode = () => {
+    setIsDrawingMode(!isDrawingMode);
+  };
 
   useEffect(() => {
     setEditorHtml(content);
@@ -25,13 +33,20 @@ const Notes = ({ content, onContentChange, onDeleteNote }) => {
 
   return (
     <div className="notes-container">
-      <ReactQuill
-        value={editorHtml}
-        onChange={handleChange}
-        modules={{ toolbar: toolbarOptions }}
-        theme="snow"
-      />
-    </div>
+      <button onClick={handleToggleDrawingMode}>
+        {isDrawingMode ? 'Retour à l\'édition' : 'Dessiner'}
+      </button>
+      {isDrawingMode ? (
+        <Tldraw />
+      ) : (
+        <ReactQuill
+          value={editorHtml}
+          onChange={handleChange}
+          modules={{ toolbar: toolbarOptions }}
+          theme="snow"
+        />
+      )}
+    </div>  // Ajout de la balise de fermeture </div>
   );
 };
 
